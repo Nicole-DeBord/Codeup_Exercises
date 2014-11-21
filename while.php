f58dacee48a5fc407d83fcc8f43e9a1360c07b6d
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-21 13:05:38 -0600

    Added a while exercise

---
+++ while.php
@@ -0,0 +1,18 @@
+<?php
+
+// exercise 3.1.4.
+
+$test=5;
+
+while ($test <= 15) {
+	echo "$test\n";
+	$test++;
+}
+
+//echo "Our loop is done!\n";
+
+// while (true) {
+// 	echo "never gonna stop\n";
+// }
+
+?>
