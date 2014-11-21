6e555928a7f90fb7a49cee9c2c1a1d85997d25fe
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-21 11:38:31 -0600

    Added exercise number to if_and_or.php

--- if_and_or.php
+++ if_and_or.php
@@ -1,12 +1,20 @@
 <?php
 
+// exercise 3.1.2.
+
  $x = 0;
  $y = 5;
  $z = 10;
+ // if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
+
+ // if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
 
  if ($x < $y && $y < $z) {
  	echo "{$x} < {$y} < {$z}\n";
  }
+
+
+ // repeat the if statement for $y and $z.
 
  if ($x > 0 || $x < 10) {
 	echo "$x is greater than 0 OR less than 10\n";
@@ -20,8 +28,13 @@ if ($z > 0 || $z < 10) {
 	echo "$z is greater than 0 OR less than 10\n";
 }
 
+// If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
+
+// repeat the if statement for $y and $z.
+
 if ($x > 0 && $x < 10) {
 	echo "$x is greater than 0 AND less than 10\n";
+	
 }
 
 if ($y > 0 && $y < 10) {
