1a7ea67501a4e2b863996659522f428beed671f4
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-25 15:50:17 -0600

    Added part 2 of a break and continue exercise

--- break_continue.php
+++ break_continue.php
@@ -4,13 +4,24 @@
 
 // create a for loop that shows all even numbers between 1 and 100 using continue
 
+// for($n = 1; $n <= 100; $n++) {
+// 	echo $n . PHP_EOL;
+// 	if ($n % 2 != 0) {
+// 		continue;
+// 	}
+// 	echo "^ that is an even number" . PHP_EOL;	
+// } 
+
+// Create another for loop that counts from 1 to 100, but stops after 10 using break
+
 for($n = 1; $n <= 100; $n++) {
-	echo $n . PHP_EOL;
-	if ($n % 2 != 0) {
-		continue;
+	if ($n == 10) {
+		break;
 	}
-	echo "^ that is an even number" . PHP_EOL;	
-} 
+	echo $n . PHP_EOL;
+}
+
+echo "You reached 10!" . PHP_EOL;
 
 
 ?>
