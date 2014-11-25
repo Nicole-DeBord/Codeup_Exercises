f104e062a92bcacc12e663fae1f91b378f0c6b11
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-25 14:50:44 -0600

    Added a foreach exercise with an associative array

---
+++ foreach_books.php
@@ -0,0 +1,40 @@
+<?php
+
+// exercise 3.3.3.
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+// construct a loop that iterates through each book, then each book's keys 
+// and values - your output should contain the book's title, then list the
+// key value pairs for the data about each book:
+
+foreach ($books as $book => $details) {
+	echo $book . PHP_EOL; 
+		foreach ($details as $detail => $value) {
+			echo $detail . ": " . $value . PHP_EOL;
+		} 
+	echo "\n";
+} 
+
+?>
