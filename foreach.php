3f275010e81f4bf2eef11dd4c366ecf6e5979a84
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-25 15:52:49 -0600

    Added another code solution to foreach exercise

--- foreach.php
+++ foreach.php
@@ -6,26 +6,26 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 // construct a loop that iterates through each value and outputs its data type
 
-foreach($things as $dataType) {
-	if(is_integer($dataType)) { 
-		echo 'integer' . PHP_EOL;
-	} 
-	elseif(is_float($dataType)) {
-		echo 'float' . PHP_EOL;
-	} 
-	elseif(is_bool($dataType)) {
-		echo 'boolean' . PHP_EOL;
-	}
-	elseif(is_array($dataType)) {
-		echo 'array' . PHP_EOL;
-	}
-	elseif(is_null($dataType)) {
-		echo 'null' . PHP_EOL;
-	}
-	elseif(is_string($dataType)) {
-		echo 'string' . PHP_EOL;
-	}
-}
+// foreach($things as $dataType) {
+// 	if(is_integer($dataType)) { 
+// 		echo 'integer' . PHP_EOL;
+// 	} 
+// 	elseif(is_float($dataType)) {
+// 		echo 'float' . PHP_EOL;
+// 	} 
+// 	elseif(is_bool($dataType)) {
+// 		echo 'boolean' . PHP_EOL;
+// 	}
+// 	elseif(is_array($dataType)) {
+// 		echo 'array' . PHP_EOL;
+// 	}
+// 	elseif(is_null($dataType)) {
+// 		echo 'null' . PHP_EOL;
+// 	}
+// 	elseif(is_string($dataType)) {
+// 		echo 'string' . PHP_EOL;
+// 	}
+// }
 
 // This does the EXACT SAME THING using gettype function, but much less work 
 
@@ -34,13 +34,19 @@ foreach($things as $dataType) {
 // 	echo $value . PHP_EOL;
 // }
 
+// Hey, look - that can be simplified!: 
+
+// foreach ($things as $dataType) {
+// 	echo gettype($dataType) . PHP_EOL;
+// }
+
 // construct a loop that iterates through each value and only outputs values
 // of scalar type:
 
 
-// foreach($things as $dataType) {
-// 	if (is_scalar($dataType)) {
-// 		echo $dataType . PHP_EOL;
+// foreach($things as $thing) {
+// 	if (is_scalar($thing)) {
+// 		echo $thing . PHP_EOL;
 // 	}
 // }
 
@@ -49,14 +55,14 @@ foreach($things as $dataType) {
 // arrays:
 
 
-// foreach($things as $dataType) {
-// 	if (is_array($dataType)) {
-// 		foreach ($dataType as $value) {
+// foreach($things as $thing) {
+// 	if (is_array($thing)) {
+// 		foreach ($thing as $value) {
 // 			echo $value . PHP_EOL;
 // 		}
 // 	}
 // 	else {
-// 		echo $dataType . PHP_EOL;
+// 		echo $thing . PHP_EOL;
 // 	}
 // }
 
