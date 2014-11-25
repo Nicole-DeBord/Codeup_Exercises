513ff0a8bb31e346ba27b3eb891972a5c47eea63
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-25 15:14:28 -0600

    Added new solution using keys and foreach loop

--- foreach_books.php
+++ foreach_books.php
@@ -29,26 +29,46 @@ $books = array(
 // and values - your output should contain the book's title, then list the
 // key value pairs for the data about each book:
 
-// foreach ($books as $book => $details) {
-// 	echo $book . PHP_EOL; 
-// 		foreach ($details as $detail => $value) {
-// 			echo $detail . ": " . $value . PHP_EOL;
-// 		} 
-// 	echo "\n";
-// } 
+foreach ($books as $book => $details) {
+	echo PHP_EOL;
+	echo $book . PHP_EOL; 
+		foreach ($details as $detail => $value) {
+			echo $detail . ": " . $value . PHP_EOL;
+		} 
+} 
 
 
 // *** Update your loop to only show books that were written after 1950:
 
-foreach ($books as $book => $details) {
-	if ($details['published'] > 1950) {
-		echo $book . PHP_EOL; 	
-			foreach ($details as $detail => $value) {
-				echo $detail . ": " . $value . PHP_EOL;
-			} 
-	}
-		echo "\n";
+// foreach ($books as $book => $details) {
+// 	echo PHP_EOL;
+// 	if ($details['published'] > 1950) {
+// 		echo $book . PHP_EOL; 	
+// 			foreach ($details as $detail => $value) {
+// 				echo $detail . ": " . $value . PHP_EOL;
+// 			} 
+// 	}
 			
-} 
+// } 
+
+// Here's an alternative way to display this multidimensional array using keys
+
+foreach ($books as $title => $info) {
+
+	echo PHP_EOL;
+
+	if($info['published'] > 1950) {
+		echo "Title: $title\n";
+		echo "Published: {$info['published']}\n";
+		echo "Author: {$info['author']}\n";
+		echo "Pages: {$info['pages']}\n";
+	} 
+
+// // Here we changed the way our output appears for books written before 1950:
+
+	else {
+		echo "$title was written by {$info['author']}. It has {$info['pages']} pages. It was written by {$info['author']}.\n";
+	}
+}
 
 ?>
