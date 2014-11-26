0a29a1e8cb1aea1b7d80128eed16fda2f10b81d2
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-26 13:38:46 -0600

    Added part 1 of function exercise

---
+++ arithmetic.php
@@ -0,0 +1,26 @@
+<?php
+
+// exercise 3.5., functions, part 1
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    echo $a - $b;
+}
+
+function multiply($a, $b) {
+    echo $a * $b;
+}
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+
+echo add(10, 5) . PHP_EOL;
+echo subtract(10, 5) . PHP_EOL;
+echo multiply(10, 5) . PHP_EOL;
+echo divide(10, 5) . PHP_EOL;
+
+?>
