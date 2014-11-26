d96094869176166868a10fc26dc6ad2465d841bb
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-26 15:28:26 -0600

    Added error message function to functions, exercise 3.5.1. part 4

--- arithmetic.php
+++ arithmetic.php
@@ -1,12 +1,12 @@
 <?php
 
-// exercise 3.5.1., parts 1 - 3 (I know, I'm inconsistent)
+// exercise 3.5.1., parts 1 - 4, EVERYTHING DONE
 
 function add($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo $a + $b . PHP_EOL;
 	} else {
-		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+		echo notNumeric($a, $b);
 	}
 }
 
@@ -14,7 +14,7 @@ function subtract($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo $a - $b . PHP_EOL;
     } else {
-    	echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+    	echo notNumeric($a, $b);
     }
 }
 
@@ -22,33 +22,46 @@ function multiply($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 	    echo $a * $b . PHP_EOL;
 	} else {
-		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+		echo notNumeric($a, $b);
 	}
 }
 
 function divide($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		if ($b == 0) {
-			echo "ERROR: You entered '{$a}' and '{$b}'. Zero is not a valid divisor." . PHP_EOL;
+			echo divideZero($a, $b);
 		} else {
 	    	echo $a / $b . PHP_EOL;
 	    }
 	} else {
-		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+		echo notNumeric($a, $b);
 	}
 }
 
 function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		if ($b == 0) {
-			echo "ERROR: You entered '{$a}' and '{$b}'. Zero is not a valid divisor." . PHP_EOL;
+			echo divideZero($a, $b);
 		} else {
 			echo $a % $b . PHP_EOL;
 		}	
 	} else {
-		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+		echo notNumeric($a, $b);
 	}
  }
+
+// Create error message functions to use within functions above
+
+ function notNumeric($a, $b) {
+ 	echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+ }
+
+ function divideZero($a, $b) {
+ 		echo "ERROR: You entered '{$a}' and '{$b}'. Zero is not a valid divisor." . PHP_EOL;
+ }
+
+ 
+
 
 $num1 = 20;
 $num2 = 0;
