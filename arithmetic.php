e4363c4ce88041623a69f626f901ec9d7d210a84
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-26 15:19:37 -0600

    Exercise 3.5.2. parts 1 - 3, argument validation added

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,57 @@
 <?php
 
-// exercise 3.5., a smarter way to do all of this
+// exercise 3.5.1., parts 1 - 3 (I know, I'm inconsistent)
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	echo $a + $b . PHP_EOL;
+	} else {
+		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+	}
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	echo $a - $b . PHP_EOL;
+    } else {
+    	echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+    }
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+	    echo $a * $b . PHP_EOL;
+	} else {
+		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+	}
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		if ($b == 0) {
+			echo "ERROR: You entered '{$a}' and '{$b}'. Zero is not a valid divisor." . PHP_EOL;
+		} else {
+	    	echo $a / $b . PHP_EOL;
+	    }
+	} else {
+		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+	}
 }
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
-}
+	if (is_numeric($a) && is_numeric($b)) {
+		if ($b == 0) {
+			echo "ERROR: You entered '{$a}' and '{$b}'. Zero is not a valid divisor." . PHP_EOL;
+		} else {
+			echo $a % $b . PHP_EOL;
+		}	
+	} else {
+		echo "ERROR: You entered '{$a}' and '{$b}'. Both arguments must be numeric values." . PHP_EOL;
+	}
+ }
 
 $num1 = 20;
-$num2 = 10;
+$num2 = 0;
 
 add($num1, $num2);
 subtract($num1, $num2);
