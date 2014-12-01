bb27ac6ba73154638ecf843597e7e3a149c41d6a
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-01 10:25:30 -0600

    Added an internal function exercise

---
+++ internal_functions.php
@@ -0,0 +1,50 @@
+<?php
+
+// exercise 3.5.3.
+
+$nothing = null;
+$something = '';
+$array = array(1,2,3);
+
+
+// create a function that checks if a variable is set or empty, and display $variable_name
+
+function checkSet($variable) {
+	if (isset($variable)) {
+		echo '$variable is SET' . PHP_EOL;
+	} else {
+		echo '$variable is EMPTY' . PHP_EOL;
+	}
+}
+
+// TEST: if var $nothing is set, display '$nothing is SET'
+
+// TEST: if var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: if var $something is set, display '$something is SET'
+
+checkSet($nothing);
+checkSet($something);
+checkSet($array);
+
+
+// Serialize the array $array, and output the results - serialize outputs an array
+// as a string
+
+function serializeArray($variable) {
+	return serialize($variable);
+}
+
+// Unserialize $array, and output the results - unserialize outputs a string as an array
+
+function unserializeArray($variable) {
+	return unserialize($variable);
+}
+
+$serial = serializeArray($array);
+echo $serial . PHP_EOL;
+$unserial = unserializeArray($serial); 
+var_dump($unserial) . PHP_EOL;
+
+
+
