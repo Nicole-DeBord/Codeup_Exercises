f413473bebab3215c0d784163192995122e928f2
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-02 15:33:51 -0600

    Added my not entirely functional solution to array search function exercise

---
+++ search_arrays2.php
@@ -0,0 +1,42 @@
+<?php
+
+// first names
+$names = ['Tina', 'Bob', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function findName($name, $array) {
+	$index = array_search($name, $array);
+	if ($index === false) {
+		echo 'FALSE' . PHP_EOL;
+	} else {
+		echo 'TRUE' . PHP_EOL;
+	}
+}
+
+// Create a function that compares two arrays
+// Pass in the value we are searching for ($name), 
+// and the arrays we are searching through ($arrayOne/$arrayTwo)
+function compareArrays($name, $arrayOne, $arrayTwo) {
+	$indexOne = array_search($name, $arrayOne);
+	$indexTwo = array_search($name, $arrayTwo);
+	// Conditional - if value is found in both arrays...
+	if ($arrayOne[$indexOne] == $arrayTwo[$indexTwo]) {
+		echo "It's in both arrays!" . PHP_EOL;
+	} else {
+		echo "It's not in both arrays." . PHP_EOL;
+	}
+
+}
+
+// if one thing is true, and another thing is true - think about it!
+
+$needle = 'Bob';
+
+// we have to pass $names as an argument too, because it is not
+// defined within our function - scope!
+
+findName($needle, $names);
+compareArrays($needle, $names, $compare)
+
+?>
